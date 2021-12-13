981225ed13066c8fc54e5d62cf944e824643fe7f
hayk99 <hayk.kocharyan@sysdig.com>
2021-12-13 13:41:39 +0100

    chore: don't validate input

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,5 @@
 #!/bin/sh -l
 
-PARSERS="tf-state tf-file tf-module cloudformation crossplane pulumi-go"
-
-if echo $PARSERS | grep -w $1 > /dev/null; then
-  SCANNEROUTPUT="$(/cloud-iac-scanner -p $1 -d $GITHUB_WORKSPACE)"
-else
-  SCANNEROUTPUT="Parser name is NOT correct."
-fi
+SCANNEROUTPUT="$(/cloud-iac-scanner -p $1 -d $GITHUB_WORKSPACE)"
 
 echo "::set-output name=scanner-output::$SCANNEROUTPUT"
