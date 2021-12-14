86b298ef0404be8294fabef425ef08ab066462f2
hayk99 <hayk.kocharyan@sysdig.com>
2021-12-14 17:08:12 +0100

    feat: check if scan succeded or not and print output

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,9 @@
 
 SCANNEROUTPUT="$(/cloud-iac-scanner -p $1 -d $GITHUB_WORKSPACE)"
 
-echo "::set-output name=scanner-output::$SCANNEROUTPUT"
+if [ $? -eq 1 ]; then
+  echo "::set-output name=scanner-output::Error scanning\n$SCANNEROUTPUT"
+  exit 1
+else
+    echo "::set-output name=scanner-output::$SCANNEROUTPUT"
+fi
