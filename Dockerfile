0287002ec6e3c40c35b3d59de449bc3bdedfc1d5
hayk99 <hayk.kocharyan@sysdig.com>
2021-12-15 19:18:10 +0100

    chore: update scanner version

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM ghcr.io/sysdiglabs/cloud-iac-scanner:pr-585
+FROM ghcr.io/sysdiglabs/cloud-iac-scanner:pr-613
 
 COPY entrypoint.sh /entrypoint.sh
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,4 @@
 
 SCANNEROUTPUT="$(/cloud-iac-scanner -p $1 -d $GITHUB_WORKSPACE)"
 
-if [ $? -eq 1 ]; then
-  echo "::set-output name=scanner-output::Error scanning\n$SCANNEROUTPUT"
-  exit 1
-else
-    echo "::set-output name=scanner-output::$SCANNEROUTPUT"
-fi
+echo "::set-output name=scanner-output::$SCANNEROUTPUT"
