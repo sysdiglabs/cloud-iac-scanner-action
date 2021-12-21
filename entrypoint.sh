4c8c4a8ff04518ea8b2a1b80629fc3c8a6df734f
hayk99 <hayk.kocharyan@sysdig.com>
2021-12-21 13:22:10 +0100

    chore: fix pipe error

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@
 
 SCANNEROUTPUT="$(/cloud-iac-scanner -p $1 -d $GITHUB_WORKSPACE -f github)"
 
-while read line ; do echo $line; done << $SCANNEROUTPUT
+echo $SCANNEROUTPUT | while read line; do echo $line"lelle\n\n"; done
