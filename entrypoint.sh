2404a9bd5fb7780d3eff93e36f855b207116f37b
hayk99 <hayk.kocharyan@sysdig.com>
2021-12-22 10:25:07 +0100

    chore: add error exit

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,11 @@
 
 SCANNEROUTPUT="$(/cloud-iac-scanner -p $1 -d $GITHUB_WORKSPACE -f github)"
 
-echo "::set-output name=scanner-output::$SCANNEROUTPUT"
+if echo -$output| grep  "Hoooray" > /dev/null 2>&1; then
+  echo "::set-output name=scanner-output::$SCANNEROUTPUT"
+  exit 0
+else
+  echo "::set-output name=scanner-output::$SCANNEROUTPUT"
+  exit 1
+fi
+
